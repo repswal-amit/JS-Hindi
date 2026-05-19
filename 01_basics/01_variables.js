4d5cc37959d1c9eefa6e3f8b40e64acfb9fd0ff9
Amit Repswal <amitrepswal4@gmail.com>
2026-05-19 17:35:55 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,10 @@
 const accountId = 882475
+let accountEmail = "Amit@google.com"
+var accountpassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountpassword, accountCity])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,3 @@
+"use strict"; // Treat all js code as newer version
+
+//alert(3+3) // We are using nodejs, not browser
