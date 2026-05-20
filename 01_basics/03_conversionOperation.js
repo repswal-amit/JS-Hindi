4a9e4592562502e2a9af4ca848b399726b0860ae
Amit Repswal <amitrepswal4@gmail.com>
2026-05-20 05:40:31 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,8 +11,9 @@
 //"33abc" => NaN
 //true => 1; false => 0
 
-//let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 
@@ -22,8 +23,52 @@
 
 let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************* OPerations ***************
+
+// let value = 3
+// let nagvalue = -value
+
+// console.log(nagvalue)
+
+// console.log(2 + 2); //4
+// console.log(2 - 2); //0
+// console.log(2 * 2); //4
+// console.log(2 ** 3); //8
+// console.log(2 / 2); //1
+// console.log(2 % 2); //0
+
+// let str1 = "Hello"
+// let str2 = " Amit"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  //12
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); //12 
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log((3 +4) * 5 % 3); //2
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
 
