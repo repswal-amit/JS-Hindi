1228a907389462b5f3f622b86fc3ab6f46ecd970
Amit Repswal <amitrepswal4@gmail.com>
2026-05-20 03:30:36 +0000

    dataType

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -1,3 +1,33 @@
 "use strict"; // Treat all js code as newer version
 
 //alert(3+3) // We are using nodejs, not browser
+
+let name = "Amit"
+let age = 18
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+
+//bigint
+
+//string => ""
+
+//boolean => true/false
+
+//null => standalone value
+
+//undefind =>
+
+//symbal => unique
+
+
+//object
+
+console.log("Amit");
+
+console.log(typeof "Amit"); //Sring
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
