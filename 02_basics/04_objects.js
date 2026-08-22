d34970bc016ae33be9ed1803a0c7df4235babc68
Amit Repswal <amitrepswal4@gmail.com>
2026-08-22 12:26:24 +0530

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -58,3 +58,18 @@ console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLogged'));
+
+const course = {
+    coursename : "js in hindi",
+    price: 999,
+    courseInstructor: "Amit"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
